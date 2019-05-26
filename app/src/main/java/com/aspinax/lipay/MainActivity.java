f7866cb52288528cf26e8ffa40adc68c737466cd
kaungu <ryankitheka@gmail.com>
2019-05-26 22:32:49 +0300

    Added some visual elements to the MainActivity
    Only for the main activity. I'll add the drag up button soon

--- app/src/main/java/com/aspinax/lipay/MainActivity.java
+++ app/src/main/java/com/aspinax/lipay/MainActivity.java
@@ -1,7 +1,15 @@
 package com.aspinax.lipay;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Hide action bar and set the status bar to white.
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.WHITE);
+        }
+        getSupportActionBar().hide();
+        //Let's work
+
+        //Some Simple animation
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(5000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        rotate.setInterpolator(new LinearInterpolator());
+
+        RelativeLayout new_contact = findViewById(R.id.rotatecircle);
+
+        new_contact.startAnimation(rotate);
     }
 }
