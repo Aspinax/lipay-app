6881230c6e494e305bd4d832f7012f41652adb84
kaungu <ryankitheka@gmail.com>
2019-05-27 19:02:17 +0300

    Added Bottom Sheet plugin made some minor adjustments with the UI. Currently designing the elements for the bottom sheet and of the other pages.

--- app/src/main/java/com/aspinax/lipay/MainActivity.java
+++ app/src/main/java/com/aspinax/lipay/MainActivity.java
@@ -3,13 +3,21 @@ package com.aspinax.lipay;
 import android.app.ActionBar;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import static android.support.design.widget.BottomSheetBehavior.STATE_COLLAPSED;
+import static android.support.design.widget.BottomSheetBehavior.STATE_EXPANDED;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +25,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hide action bar and set the status bar to white.
+
+        //Bottom Sheet code for the Quick Pay feature
+        View bottomSheet = findViewById(R.id.bottom_sheet);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        bottomSheetBehavior.setState(STATE_COLLAPSED);
+        //Hide action bar and set the status bar to white and hide soft keyboard on start.
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.WHITE);
         }
         getSupportActionBar().hide();
-        //Let's work
 
+
+
+        //Let's work
+        LinearLayout request_payment_btn = findViewById(R.id.request_payment_btn);
+        request_payment_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast=Toast.makeText(getApplicationContext(),"OMG! I've been Clicked!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
         //Some Simple animation
         RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotate.setDuration(5000);
